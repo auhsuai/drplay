d0339bb2e4e2dca1266dba472f6cfe75576cee73
auhsuai <auhsuai@users.noreply.github.com>
2026-07-05 17:12:09 +0700

    Remove test and run files

--- check_size.py
+++
@@ -1,3 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('c:/Users/thinkpad/Desktop/Antigravity/drplay/music_database.db')
-print(conn.execute("SELECT length(cover_art), length(thumbnail) FROM tracks WHERE id = 'fc62e0fcc304a2da76bcba5ddeb887fb'").fetchall())
